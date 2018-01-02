c49b5e77b94cac99831311c98962e77dd6d01814
ZZY <340032382@qq.com>
2018-01-02 20:07:19 +0800

    初始化项目

---
+++ ChildTrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChildTrack
+//
+//  Created by zzy on 02/01/2018.
+//  Copyright © 2018 zzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
