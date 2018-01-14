be411d80dfc309ddc37644226decf966cf9910dd
ZZY <340032382@qq.com>
2018-01-14 18:44:03 +0800

    按钮点击事件

--- ChildTrack/Controller/MainViewController.m
+++ ChildTrack/Controller/MainViewController.m
@@ -26,6 +26,8 @@
     [introduceBtn setTitle:@"介绍" forState:UIControlStateNormal];
     introduceBtn.layer.cornerRadius = 10;
     introduceBtn.frame = CGRectMake(Margin, Margin, ScreenW - 2*Margin, (ScreenH - 5*Margin)/10);
+    introduceBtn.tag = 101;
+    [introduceBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:introduceBtn];
     
     UIButton *myLocationBtn = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -34,6 +36,8 @@
     myLocationBtn.titleLabel.font = [UIFont boldSystemFontOfSize:20];
     myLocationBtn.layer.cornerRadius = 10;
     myLocationBtn.frame = CGRectMake(Margin, introduceBtn.bottom+Margin, ScreenW - 2*Margin, (ScreenH - 5*Margin)/5);
+    introduceBtn.tag = 102;
+    [myLocationBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:myLocationBtn];
     
     UIButton *trackedBtn = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -42,6 +46,8 @@
     trackedBtn.titleLabel.font = [UIFont boldSystemFontOfSize:20];
     trackedBtn.layer.cornerRadius = 10;
     trackedBtn.frame = CGRectMake(Margin, myLocationBtn.bottom+Margin, ScreenW - 2*Margin, (ScreenH - 5*Margin)*3/10);
+    introduceBtn.tag = 103;
+    [trackedBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:trackedBtn];
     
     UIButton *trackBtn = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -50,6 +56,8 @@
     trackBtn.titleLabel.font = [UIFont boldSystemFontOfSize:20];
     trackBtn.layer.cornerRadius = 10;
     trackBtn.frame = CGRectMake(Margin, trackedBtn.bottom+Margin, ScreenW - 2*Margin, (ScreenH - 5*Margin)*2/5);
+    introduceBtn.tag = 104;
+    [trackBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:trackBtn];
     
     
@@ -65,6 +73,25 @@
     [self.navigationController setNavigationBarHidden:NO animated:YES];
 }
 
+- (void)btnClick:(UIButton *)sender {
+    
+    switch (sender.tag) {
+        case 101:
+            
+            break;
+        case 102:
+            
+            break;
+        case 103:
+            
+            break;
+        case 104:
+            
+            break;
+        default:
+            break;
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
