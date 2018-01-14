c14961c876f5e41fe748a726a35c35b0fbe6b232
ZZY <340032382@qq.com>
2018-01-14 21:25:48 +0800

    增加追踪他人页面

---
+++ ChildTrack/Controller/TrackViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrackViewController.h
+//  ChildTrack
+//
+//  Created by zzy on 14/01/2018.
+//  Copyright © 2018 zzy. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface TrackViewController : BaseViewController
+
+@end
---
+++ ChildTrack/Controller/TrackViewController.m
@@ -0,0 +1,37 @@
+//
+//  TrackViewController.m
+//  ChildTrack
+//
+//  Created by zzy on 14/01/2018.
+//  Copyright © 2018 zzy. All rights reserved.
+//
+
+#import "TrackViewController.h"
+
+@interface TrackViewController ()
+
+@end
+
+@implementation TrackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
